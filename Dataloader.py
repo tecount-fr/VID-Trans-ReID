18b4a0afbdc187905a5dfaddf78dcb1a9595c9e0
Florian Ringelhäuser <florian.ringelhaeuser@th-deg.de>
2023-11-02 23:10:07 +0100

    fix: fix visualization

--- Dataloader.py
+++ Dataloader.py
@@ -11,43 +11,43 @@ import numpy as np
 import math
 
 from timm.data.random_erasing import RandomErasing
-from utility import RandomIdentitySampler,RandomErasing3
+from utility import RandomIdentitySampler, RandomErasing3
 from Datasets.MARS_dataset import Mars
 from Datasets.iLDSVID import iLIDSVID
 from Datasets.PRID_dataset import PRID
 
 __factory = {
-    'Mars':Mars,
-    'iLIDSVID':iLIDSVID,
-    'PRID':PRID
+    'Mars': Mars,
+    'iLIDSVID': iLIDSVID,
+    'PRID': PRID
 }
 
+
 def train_collate_fn(batch):
-    
-    imgs, pids, camids,a= zip(*batch)
+    imgs, pids, camids, a = zip(*batch)
     pids = torch.tensor(pids, dtype=torch.int64)
-    
+
     camids = torch.tensor(camids, dtype=torch.int64)
     return torch.stack(imgs, dim=0), pids, camids, torch.stack(a, dim=0)
 
+
 def val_collate_fn(batch):
-    
     imgs, pids, camids, img_paths = zip(*batch)
     viewids = torch.tensor(viewids, dtype=torch.int64)
     camids_batch = torch.tensor(camids, dtype=torch.int64)
-    return torch.stack(imgs, dim=0), pids, camids_batch,  img_paths
+    return torch.stack(imgs, dim=0), pids, camids_batch, img_paths
+
 
 def dataloader(Dataset_name):
     train_transforms = T.Compose([
-            T.Resize([256, 128], interpolation=3),
-            T.RandomHorizontalFlip(p=0.5),
-            T.Pad(10),
-            T.RandomCrop([256, 128]),
-            T.ToTensor(),
-            T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-            
-            
-        ])
+        T.Resize([256, 128], interpolation=3),
+        T.RandomHorizontalFlip(p=0.5),
+        T.Pad(10),
+        T.RandomCrop([256, 128]),
+        T.ToTensor(),
+        T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+
+    ])
 
     val_transforms = T.Compose([
         T.Resize([256, 128]),
@@ -55,23 +55,26 @@ def dataloader(Dataset_name):
         T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
     ])
 
-    
-
     dataset = __factory[Dataset_name]()
-    train_set = VideoDataset_inderase(dataset.train, seq_len=4, sample='intelligent',transform=train_transforms)
+    train_set = VideoDataset_inderase(dataset.train, seq_len=4,
+                                      sample='intelligent',
+                                      transform=train_transforms)
     num_classes = dataset.num_train_pids
     cam_num = dataset.num_train_cams
     view_num = dataset.num_train_vids
 
-   
-    train_loader = DataLoader(train_set, batch_size=16,sampler=RandomIdentitySampler(dataset.train, 64,4),num_workers=4, collate_fn=train_collate_fn)
-  
-    q_val_set = VideoDataset(dataset.query, seq_len=4, sample='dense', transform=val_transforms)
-    g_val_set = VideoDataset(dataset.gallery, seq_len=4, sample='dense', transform=val_transforms)
-    
-    
-    return train_loader, len(dataset.query), num_classes, cam_num, view_num,q_val_set,g_val_set
+    train_loader = DataLoader(train_set, batch_size=64,
+                              sampler=RandomIdentitySampler(dataset.train, 64,
+                                                            4), num_workers=4,
+                              collate_fn=train_collate_fn)
 
+    q_val_set = VideoDataset(dataset.query, seq_len=4, sample='dense',
+                             transform=val_transforms)
+    g_val_set = VideoDataset(dataset.gallery, seq_len=4, sample='dense',
+                             transform=val_transforms)
+
+    return train_loader, len(
+        dataset.query), num_classes, cam_num, view_num, q_val_set, g_val_set
 
 
 def read_image(img_path):
@@ -83,9 +86,12 @@ def read_image(img_path):
             img = Image.open(img_path).convert('RGB')
             got_img = True
         except IOError:
-            print("IOError incurred when reading '{}'. Will redo. Don't worry. Just chill.".format(img_path))
+            print(
+                "IOError incurred when reading '{}'. Will redo. Don't worry. Just chill.".format(
+                    img_path))
             pass
     return img
+
 
 class VideoDataset(Dataset):
     """Video Person ReID Dataset.
@@ -93,7 +99,8 @@ class VideoDataset(Dataset):
     """
     sample_methods = ['evenly', 'random', 'all']
 
-    def __init__(self, dataset, seq_len=15, sample='evenly', transform=None , max_length=40):
+    def __init__(self, dataset, seq_len=15, sample='evenly', transform=None,
+                 max_length=1):
         self.dataset = dataset
         self.seq_len = seq_len
         self.sample = sample
@@ -109,10 +116,9 @@ class VideoDataset(Dataset):
 
         # if self.sample == 'restricted_random':
         #     frame_indices = range(num)
-        #     chunks = 
+        #     chunks =
         #     rand_end = max(0, len(frame_indices) - self.seq_len - 1)
         #     begin_index = random.randint(0, rand_end)
-
 
         if self.sample == 'random':
             """
@@ -128,14 +134,15 @@ class VideoDataset(Dataset):
             indices = frame_indices[begin_index:end_index]
             # print(begin_index, end_index, indices)
             if len(indices) < self.seq_len:
-                indices=np.array(indices)
-                indices = np.append(indices , [indices[-1] for i in range(self.seq_len - len(indices))])
+                indices = np.array(indices)
+                indices = np.append(indices, [indices[-1] for i in range(
+                    self.seq_len - len(indices))])
             else:
-                indices=np.array(indices)
+                indices = np.array(indices)
             imgs = []
-            targt_cam=[]
+            targt_cam = []
             for index in indices:
-                index=int(index)
+                index = int(index)
                 img_path = img_paths[index]
                 img = read_image(img_path)
                 if self.transform is not None:
@@ -144,7 +151,7 @@ class VideoDataset(Dataset):
                 targt_cam.append(camid)
                 imgs.append(img)
             imgs = torch.cat(imgs, dim=0)
-            #imgs=imgs.permute(1,0,2,3)
+            # imgs=imgs.permute(1,0,2,3)
             return imgs, pid, targt_cam
 
         elif self.sample == 'dense':
@@ -154,31 +161,31 @@ class VideoDataset(Dataset):
             """
             # import pdb
             # pdb.set_trace()
-        
-            cur_index=0
+
+            cur_index = 0
             frame_indices = [i for i in range(num)]
-            indices_list=[]
-            while num-cur_index > self.seq_len:
-                indices_list.append(frame_indices[cur_index:cur_index+self.seq_len])
-                cur_index+=self.seq_len
-            last_seq=frame_indices[cur_index:]
+            indices_list = []
+            while num - cur_index > self.seq_len:
+                indices_list.append(
+                    frame_indices[cur_index:cur_index + self.seq_len])
+                cur_index += self.seq_len
+            last_seq = frame_indices[cur_index:]
             # print(last_seq)
             for index in last_seq:
                 if len(last_seq) >= self.seq_len:
                     break
                 last_seq.append(index)
-            
 
             indices_list.append(last_seq)
-            imgs_list=[]
-            targt_cam=[]
+            imgs_list = []
+            targt_cam = []
             # print(indices_list , num , img_paths  )
             for indices in indices_list:
                 if len(imgs_list) > self.max_length:
-                    break 
+                    break
                 imgs = []
                 for index in indices:
-                    index=int(index)
+                    index = int(index)
                     img_path = img_paths[index]
                     img = read_image(img_path)
                     if self.transform is not None:
@@ -187,11 +194,11 @@ class VideoDataset(Dataset):
                     imgs.append(img)
                     targt_cam.append(camid)
                 imgs = torch.cat(imgs, dim=0)
-                #imgs=imgs.permute(1,0,2,3)
+                # imgs=imgs.permute(1,0,2,3)
                 imgs_list.append(imgs)
             imgs_array = torch.stack(imgs_list)
-            return imgs_array, pid, targt_cam,img_paths
-            #return imgs_array, pid, int(camid),trackid
+            return imgs_array, pid, targt_cam, img_paths
+            # return imgs_array, pid, int(camid),trackid
 
 
         elif self.sample == 'dense_subset':
@@ -202,31 +209,30 @@ class VideoDataset(Dataset):
             frame_indices = range(num)
             rand_end = max(0, len(frame_indices) - self.max_length - 1)
             begin_index = random.randint(0, rand_end)
-            
 
-            cur_index=begin_index
+            cur_index = begin_index
             frame_indices = [i for i in range(num)]
-            indices_list=[]
-            while num-cur_index > self.seq_len:
-                indices_list.append(frame_indices[cur_index:cur_index+self.seq_len])
-                cur_index+=self.seq_len
-            last_seq=frame_indices[cur_index:]
+            indices_list = []
+            while num - cur_index > self.seq_len:
+                indices_list.append(
+                    frame_indices[cur_index:cur_index + self.seq_len])
+                cur_index += self.seq_len
+            last_seq = frame_indices[cur_index:]
             # print(last_seq)
             for index in last_seq:
                 if len(last_seq) >= self.seq_len:
                     break
                 last_seq.append(index)
-            
 
             indices_list.append(last_seq)
-            imgs_list=[]
+            imgs_list = []
             # print(indices_list , num , img_paths  )
             for indices in indices_list:
                 if len(imgs_list) > self.max_length:
-                    break 
+                    break
                 imgs = []
                 for index in indices:
-                    index=int(index)
+                    index = int(index)
                     img_path = img_paths[index]
                     img = read_image(img_path)
                     if self.transform is not None:
@@ -234,24 +240,27 @@ class VideoDataset(Dataset):
                     img = img.unsqueeze(0)
                     imgs.append(img)
                 imgs = torch.cat(imgs, dim=0)
-                #imgs=imgs.permute(1,0,2,3)
+                # imgs=imgs.permute(1,0,2,3)
                 imgs_list.append(imgs)
             imgs_array = torch.stack(imgs_list)
             return imgs_array, pid, camid
-        
+
         elif self.sample == 'intelligent_random':
             # frame_indices = range(num)
             indices = []
-            each = max(num//seq_len,1)
-            for  i in range(seq_len):
-                if i != seq_len -1:
-                    indices.append(random.randint(min(i*each , num-1), min( (i+1)*each-1, num-1)) )
+            each = max(num // self.seq_len, 1)
+            for i in range(self.seq_len):
+                if i != self.seq_len - 1:
+                    indices.append(random.randint(min(i * each, num - 1),
+                                                  min((i + 1) * each - 1,
+                                                      num - 1)))
                 else:
-                    indices.append(random.randint(min(i*each , num-1), num-1) )
+                    indices.append(
+                        random.randint(min(i * each, num - 1), num - 1))
             print(len(indices))
             imgs = []
             for index in indices:
-                index=int(index)
+                index = int(index)
                 img_path = img_paths[index]
                 img = read_image(img_path)
                 if self.transform is not None:
@@ -259,27 +268,29 @@ class VideoDataset(Dataset):
                 img = img.unsqueeze(0)
                 imgs.append(img)
             imgs = torch.cat(imgs, dim=0)
-            #imgs=imgs.permute(1,0,2,3)
+            # imgs=imgs.permute(1,0,2,3)
             return imgs, pid, camid
         else:
-            raise KeyError("Unknown sample method: {}. Expected one of {}".format(self.sample, self.sample_methods))
+            raise KeyError(
+                "Unknown sample method: {}. Expected one of {}".format(
+                    self.sample, self.sample_methods))
 
-            
 
-        
 class VideoDataset_inderase(Dataset):
     """Video Person ReID Dataset.
     Note batch data has shape (batch, seq_len, channel, height, width).
     """
     sample_methods = ['evenly', 'random', 'all']
 
-    def __init__(self, dataset, seq_len=15, sample='evenly', transform=None , max_length=40):
+    def __init__(self, dataset, seq_len=15, sample='evenly', transform=None,
+                 max_length=4):
         self.dataset = dataset
         self.seq_len = seq_len
         self.sample = sample
         self.transform = transform
         self.max_length = max_length
-        self.erase = RandomErasing3(probability=0.5, mean=[0.485, 0.456, 0.406])
+        self.erase = RandomErasing3(probability=0.5,
+                                    mean=[0.485, 0.456, 0.406])
 
     def __len__(self):
         return len(self.dataset)
@@ -299,36 +310,39 @@ class VideoDataset_inderase(Dataset):
                 if len(indices1) >= self.seq_len:
                     break
                 indices.append(index)
-            indices=np.array(indices)
+            indices = np.array(indices)
         else:
             # frame_indices = range(num)
             indices = []
-            each = max(num//self.seq_len,1)
-            for  i in range(self.seq_len):
-                if i != self.seq_len -1:
-                    indices.append(random.randint(min(i*each , num-1), min( (i+1)*each-1, num-1)) )
+            each = max(num // self.seq_len, 1)
+            for i in range(self.seq_len):
+                if i != self.seq_len - 1:
+                    indices.append(random.randint(min(i * each, num - 1),
+                                                  min((i + 1) * each - 1,
+                                                      num - 1)))
                 else:
-                    indices.append(random.randint(min(i*each , num-1), num-1) )
+                    indices.append(
+                        random.randint(min(i * each, num - 1), num - 1))
             # print(len(indices), indices, num )
         imgs = []
         labels = []
-        targt_cam=[]
-        
+        targt_cam = []
+
         for index in indices:
-            index=int(index)
+            index = int(index)
             img_path = img_paths[index]
-            
+
             img = read_image(img_path)
             if self.transform is not None:
                 img = self.transform(img)
-            img , temp  = self.erase(img)
+            img, temp = self.erase(img)
             labels.append(temp)
             img = img.unsqueeze(0)
             imgs.append(img)
             targt_cam.append(camid)
         labels = torch.tensor(labels)
         imgs = torch.cat(imgs, dim=0)
-        
-        return imgs, pid, targt_cam ,labels
-        
+
+        return imgs, pid, targt_cam, labels
+
 
--- VID_Test.py
+++ VID_Test.py
@@ -1,6 +1,6 @@
 from Dataloader import dataloader
 from VID_Trans_model import VID_Trans
-
+from visualize_results import visualize_ranked_results
 
 from Loss_fun import make_loss
 import random
@@ -9,6 +9,8 @@ import numpy as np
 import os
 import argparse
 
+import pickle
+
 import logging
 import os
 import time
@@ -16,22 +18,17 @@ import torch
 import torch.nn as nn
 
 from torch.cuda import amp
-from utility import AverageMeter, optimizer,scheduler
+from utility import AverageMeter, optimizer, scheduler
 
-from torch.autograd import Variable   
-
-
-
-
-
-
+from torch.autograd import Variable
 
 
 def evaluate(distmat, q_pids, g_pids, q_camids, g_camids, max_rank=21):
     num_q, num_g = distmat.shape
     if num_g < max_rank:
         max_rank = num_g
-        print("Note: number of gallery samples is quite small, got {}".format(num_g))
+        print("Note: number of gallery samples is quite small, got {}".format(
+            num_g))
     indices = np.argsort(distmat, axis=1)
     matches = (g_pids[indices] == q_pids[:, np.newaxis]).astype(np.int32)
 
@@ -50,7 +47,8 @@ def evaluate(distmat, q_pids, g_pids, q_camids, g_camids, max_rank=21):
         keep = np.invert(remove)
 
         # compute cmc curve
-        orig_cmc = matches[q_idx][keep] # binary vector, positions with value 1 are correct matches
+        orig_cmc = matches[q_idx][
+            keep]  # binary vector, positions with value 1 are correct matches
         if not np.any(orig_cmc):
             # this condition is true when query identity does not appear in gallery
             continue
@@ -65,7 +63,7 @@ def evaluate(distmat, q_pids, g_pids, q_camids, g_camids, max_rank=21):
         # reference: https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Average_precision
         num_rel = orig_cmc.sum()
         tmp_cmc = orig_cmc.cumsum()
-        tmp_cmc = [x / (i+1.) for i, x in enumerate(tmp_cmc)]
+        tmp_cmc = [x / (i + 1.) for i, x in enumerate(tmp_cmc)]
         tmp_cmc = np.asarray(tmp_cmc) * orig_cmc
         AP = tmp_cmc.sum() / num_rel
         all_AP.append(AP)
@@ -78,71 +76,132 @@ def evaluate(distmat, q_pids, g_pids, q_camids, g_camids, max_rank=21):
 
     return all_cmc, mAP
 
-def test(model, queryloader, galleryloader, pool='avg', use_gpu=True, ranks=[1, 5, 10, 20]):
+
+def test(model, queryloader, galleryloader, pool='avg', use_gpu=True,
+         ranks=[1, 5, 10, 20]):
+    # query_loader = q_val_set,            gallery_loader = g_val_set
+    # q_val_set, g_val_set  coming from dataloader('Mars')
+    # q_val_set = VideoDataset()
     model.eval()
-    qf, q_pids, q_camids = [], [], []
+    qf, q_pids, q_camids, q_img_paths = [], [], [], []
+
     with torch.no_grad():
-      for batch_idx, (imgs, pids, camids,_) in enumerate(queryloader):
-       
-        if use_gpu:
-            imgs = imgs.cuda()
-        imgs = Variable(imgs, volatile=True)
-        
-        b,  s, c, h, w = imgs.size()
-        
-        
-        features = model(imgs,pids,cam_label=camids )
-       
-        features = features.view(b, -1)
-        features = torch.mean(features, 0)
-        features = features.data.cpu()
-        qf.append(features)
-        
-        q_pids.append(pids)
-        q_camids.extend(camids)
-      qf = torch.stack(qf)
-      q_pids = np.asarray(q_pids)
-      q_camids = np.asarray(q_camids)
-      print("Extracted features for query set, obtained {}-by-{} matrix".format(qf.size(0), qf.size(1)))
-      gf, g_pids, g_camids = [], [], []
-      for batch_idx, (imgs, pids, camids,_) in enumerate(galleryloader):
-        if use_gpu:
-            imgs = imgs.cuda()
-        imgs = Variable(imgs, volatile=True)
-        b, s,c, h, w = imgs.size()
-        features = model(imgs,pids,cam_label=camids)
-        features = features.view(b, -1)
-        if pool == 'avg':
+        for batch_idx, (imgs, pids, camids, img_paths) in enumerate(
+                queryloader):
+            print(batch_idx)
+            if use_gpu:
+                Q_imgs = imgs.cuda()
+            # imgs = Variable(imgs, volatile=True)
+            with torch.no_grad():
+                Q_imgs = Variable(Q_imgs)
+
+            b, s, c, h, w = Q_imgs.size()
+
+            features = model(Q_imgs, pids, cam_label=camids)
+            features = features.view(b, -1)
             features = torch.mean(features, 0)
-        else:
-            features, _ = torch.max(features, 0)
-        features = features.data.cpu()
-        gf.append(features)
-        g_pids.append(pids)
-        g_camids.extend(camids)
-    gf = torch.stack(gf)
-    g_pids = np.asarray(g_pids)
-    g_camids = np.asarray(g_camids)
-    print("Extracted features for gallery set, obtained {}-by-{} matrix".format(gf.size(0), gf.size(1)))
+            features = features.data.cpu()
+            qf.append(features)
+
+            q_pids.append(pids)
+            q_camids.extend(camids)
+            q_img_paths.extend(img_paths)
+
+        qf = torch.stack(qf)
+        q_pids = np.asarray(q_pids)
+        q_camids = np.asarray(q_camids)
+        q_img_paths = np.asarray(q_img_paths)
+        print(
+            "Extracted features for query set, obtained {}-by-{} matrix".format(
+                qf.size(0), qf.size(1)))
+
+        gf, g_pids, g_camids, g_img_paths = [], [], [], []
+
+        for batch_idx, (imgs, pids, camids, img_paths) in enumerate(
+                galleryloader):
+            print(batch_idx)
+            if use_gpu:
+                G_imgs = imgs.cuda()
+
+            # imgs = Variable(imgs, volatile=True)
+            with torch.no_grad():
+                G_imgs = Variable(G_imgs)
+
+            b, s, c, h, w = G_imgs.size()
+            features = model(G_imgs, pids, cam_label=camids)
+            features = features.view(b, -1)
+            if pool == 'avg':
+                features = torch.mean(features, 0)
+            else:
+                features, _ = torch.max(features, 0)
+            features = features.data.cpu()
+            gf.append(features)
+            g_pids.append(pids)
+            g_camids.extend(camids)
+            g_img_paths.extend(img_paths)
+
+        gf = torch.stack(gf)
+        g_pids = np.asarray(g_pids)
+        g_camids = np.asarray(g_camids)
+        g_img_paths = np.asarray(g_img_paths)
+        print(
+            "Extracted features for gallery set, obtained {}-by-{} matrix".format(
+                gf.size(0), gf.size(1)))
+
+        # Create a dictionary to store the data
+        data = {
+            'qf': qf,  # Query features
+            'q_pids': q_pids,  # Query person IDs
+            'q_camids': q_camids,  # Query camera IDs
+            'q_img_paths': q_img_paths,  # Query image paths
+            'gf': gf,  # Gallery features
+            'g_pids': g_pids,  # Gallery person IDs
+            'g_camids': g_camids,  # Gallery camera IDs
+            'g_img_paths': g_img_paths,  # Gallery image paths
+        }
+
+        # Specify the path where you want to save the pickle file
+        pickle_file_path = 'feature_data.pkl'
+
+        # Save the data to a pickle file
+        with open(pickle_file_path, 'wb') as file:
+            pickle.dump(data, file)
+
+        print(f"Features and related information saved to {pickle_file_path}")
+
     print("Computing distance matrix")
     m, n = qf.size(0), gf.size(0)
-    distmat = torch.pow(qf, 2).sum(dim=1, keepdim=True).expand(m, n) +               torch.pow(gf, 2).sum(dim=1, keepdim=True).expand(n, m).t()
+    distmat = torch.pow(qf, 2).sum(dim=1, keepdim=True).expand(m,
+                                                               n) + torch.pow(
+        gf, 2).sum(dim=1, keepdim=True).expand(n, m).t()
     distmat.addmm_(1, -2, qf, gf.t())
     distmat = distmat.numpy()
     gf = gf.numpy()
     qf = qf.numpy()
-    
+
     print("Original Computing CMC and mAP")
     cmc, mAP = evaluate(distmat, q_pids, g_pids, q_camids, g_camids)
-    
+
     # print("Results ---------- {:.1%} ".format(distmat_rerank))
     print("Results ---------- ")
-    
-    print("mAP: {:.1%} ".format(mAP))
-    print("CMC curve r1:",cmc[0])
-    
-    return cmc[0], mAP
 
+    print("mAP: {:.1%} ".format(mAP))
+    print("CMC curve r1:", cmc[0])
+
+    # TO visualize the results - on MARS
+    # Get the query and gallery data
+    # query_data = (q_img_paths, q_pids, q_camids)
+    # gallery_data =(g_img_paths, g_pids, g_camids)
+
+    # img_paths, pid, camid = self.dataset[index]
+
+    # Visualize the results
+    print(f"visualizing results for {Dataset_name} has started now!")
+    visualize_ranked_results(distmat, (queryloader, galleryloader), "video",
+                             width=128, height=256, save_dir='', topk=10)
+    print("Visualization of ranked results finished (supposedly) successfully")
+
+    return cmc[0], mAP
 
 
 if __name__ == '__main__':
@@ -150,24 +209,23 @@ if __name__ == '__main__':
     parser.add_argument(
         "--Dataset_name", default="", help="The name of the DataSet", type=str)
     parser.add_argument(
-        "--model_path", default="", help="pretrained model", type=str)    
+        "--model_path", default="", help="pretrained model", type=str)
     args = parser.parse_args()
-    Dataset_name=args.Dataset_name
-    pretrainpath=args.model_path
+    Dataset_name = args.Dataset_name
+    pretrainpath = args.model_path
 
-  
-
-    train_loader,  num_query, num_classes, camera_num, view_num,q_val_set,g_val_set = dataloader(Dataset_name)
-    model = VID_Trans( num_classes=num_classes, camera_num=camera_num,pretrainpath=None)
+    train_loader, num_query, num_classes, camera_num, view_num, q_val_set, g_val_set = dataloader(
+        Dataset_name)
+    model = VID_Trans(num_classes=num_classes, camera_num=camera_num,
+                      pretrainpath=pretrainpath)
 
     device = "cuda"
-    model=model.to(device)
+    model = model.to(device)
 
     checkpoint = torch.load(pretrainpath)
     model.load_state_dict(checkpoint)
 
-    
     model.eval()
-    cmc,map = test(model, q_val_set,g_val_set)
-    print('CMC: %.4f, mAP : %.4f'%(cmc,map))
-    
+    cmc, map = test(model, q_val_set, g_val_set)
+    print('CMC: %.4f, mAP : %.4f' % (cmc, map))
+
